2aa9adc4ec2d34c055e4a6f41d9ecdaf58e905ec
Milan <milan.dhar@gmail.com>
2019-03-05 11:36:47 -0800

    my select

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,21 @@
 def my_select(collection)
  # your code here!
+if block_given?
+
+ i = 0
+ counter = 0
+ empty = []
+
+ while i < collection.length
+   if (yield collection[i]) == true
+     empty[counter] = collection[i]
+     counter += 1
+   end
+   i += 1
+ end
+
+empty
+else
+  puts "This block should not run!"
+end
 end
