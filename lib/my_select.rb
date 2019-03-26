22bb9c59720fff53c344a6a5ea6d30b646d8cbd1
Alex Kitelinger <bureaucratix@gmail.com>
2019-03-26 15:16:34 -0700

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
-end
+  def my_select(collection)
+    i=0
+    new_array=[]
+    while i < collection.length
+      if yield collection[i]
+        new_array.push collection[i]
+      end
+      i+=1
+    end
+    new_array
+  end
