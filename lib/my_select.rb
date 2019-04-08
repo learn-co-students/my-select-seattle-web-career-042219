7142b57572863969366b8d972cf62081416c3644
Danny Tseng <dtfocus1025@gmail.com>
2019-04-08 19:06:09 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
  # your code here!
+ new = []
+ if collection.size > 0
+  i = 0
+  while i < collection.size
+    #result = yield collection[i]
+    if yield collection[i] #== true
+      new.push(collection[i])
+    end
+    i += 1
+  end
+ end
+ new
 end
